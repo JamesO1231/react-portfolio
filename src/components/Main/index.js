a7ae1c6cc55ab295bf8c7aa94de2c58539996b8c
James Odell <jpodell21@gmail.com>
2021-09-15 17:42:17 -0600

    first commit

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import About from '../About';
+import Feedback from '../Feedback'
+
+export default function Main() {
+    return (
+        <div>
+            <Navbar/>
+            <Feedback/>
+        </div>
+    )
+}
